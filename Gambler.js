e56a2de96d5a7cd0d8f6b1bb1f5a704ca3482865
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-06-03 18:32:14 +0530

    Added file of UC2_HEAD_TAIL_WINS

--- Gambler.js
+++ Gambler.js
@@ -1,26 +1,16 @@
-console.log("================== Welcome To Gambler Game ========================")
+console.log("======= Coin flip to win if Head/Tail comes 11 times ============")
 
-//Constants declaration
-var StackMoney = 100
-const StackGoal = 200
-//Variables declaration
-var Win = 0
-var Lose = 0
-console.log("Intial Money is 100.")
-while ((StackMoney < StackGoal) && (StackMoney > 0)) {
-    var BetPlay = Math.floor(Math.random() * 2)
-    if (BetPlay == 0) {
-        StackMoney++
-        Win++
-    }
-    else {
-        StackMoney--
-        Lose++
-    }
+let HeadCount = 0;
+let TailCount = 0;
+while (HeadCount <= 11 && TailCount <= 11) 
+{
+    let CoinToss = Math.floor(Math.random() * 10) % 2;
+    if (CoinToss == 1)
+        HeadCount++;
+    else
+        TailCount++;
 }
-if ((StackMoney == 200) && (StackMoney == 0)) {
-    console.log("Gambler has won the game!!")
-} else {
-    console.log("Gambler has lose  the game.")
-}
-console.log("Final Money After Game : " + StackMoney +"\n"+ "No Of Win Times : " + Win +"\n"+ "No Of  Bet Times : " +(Win+Lose))
+if (TailCount == 11)
+    console.log("Tail won!! It comes 11 times.");
+else
+    console.log("Head won!! It comes 11 times.");
